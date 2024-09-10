e2e0a1b963da9710eb4cb837f6ea3e2cf45598d3
Sai-Karthik9113 <sai9898karthik@gmail.com>
2024-09-10 19:04:03 +0530

    Add screenshots and update README.md

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [isRunning, setIsRunning] = useState(false);
+  const [minutes, setMinutes] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    let timer;
+
+    if(isRunning) {
+      timer = setInterval(() => {
+        setSeconds((prevSeconds) => {
+          if (prevSeconds === 59) {
+            setMinutes((prevMinutes) => prevMinutes + 1);
+            return 0;
+          }
+          return prevSeconds + 1;
+        });
+      }, 1000);
+    }
+
+    return () => clearInterval(timer);
+  }, [isRunning]);
+
+
+  const handleStartStopBtn = () => {
+    setIsRunning(!isRunning);
+  }
+
+  const handleResetBtn = () => {
+    setIsRunning(false);
+    setMinutes(0);
+    setSeconds(0);
+  }
+
+  const timeFormat = (val, isMinutes = false) => {
+    if (isMinutes) {
+      return val;
+    }
+    return String(val).padStart(2, '0');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Stopwatch</h1>
+      <p>Time: {timeFormat(minutes, true)}:{timeFormat(seconds)}</p>
+      <div className='controllers'>
+        <button onClick={handleStartStopBtn}>{!isRunning ? 'Start' : 'Stop'}</button>
+        <button onClick={handleResetBtn}>Reset</button>
+      </div>
     </div>
   );
 }
